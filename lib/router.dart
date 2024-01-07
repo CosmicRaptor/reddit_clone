d8c6b856b32213b2456b5cf8ce6506a63537d69f
Aryan <aupathak1234@gmail.com>
2024-01-07 23:27:09 +0530

    added ability to create communities

--- lib/router.dart
+++ lib/router.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/features/community/screens/create_community.dart';
 import 'package:routemaster/routemaster.dart';
 
 import 'features/home/screens/home_screen.dart';
@@ -11,5 +12,6 @@ final loggedOutRoutes = RouteMap(routes: {
 });
 
 final loggedInRoutes = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: HomeScreen())
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-community': (_) => const MaterialPage(child: CreateCommunityScreen())
 });
