d9a73eef769b8cacc26490a51e2fc5a45b0f5e80
Aryan <aupathak1234@gmail.com>
2024-01-18 14:45:12 +0530

    added blur to some components

--- lib/features/home/drawers/community_list_drawer.dart
+++ lib/features/home/drawers/community_list_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/common/error_text.dart';
@@ -20,32 +22,35 @@ class CommunityListDrawer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Drawer(
-      child: SafeArea(
-        child: Column(
-          children: [
-            ListTile(
-              title: const Text('Create community'),
-              leading: const Icon(Icons.add),
-              onTap: () => navigateToCreateCommunity(context),
-            ),
-            
-            ref.watch(userCommunitiesProvider).when(data: (communities) => Expanded(
-              child: ListView.builder(
-                itemCount: communities.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final community = communities[index];
-                  return ListTile(
-                    title: Text('c/${community.name}'),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(community.avatar),
-                    ),
-                    onTap: (){navigateToCommunity(context, community);},
-                  );
-                },
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaY: 10, sigmaX: 10),
+      child: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: [
+              ListTile(
+                title: const Text('Create community'),
+                leading: const Icon(Icons.add),
+                onTap: () => navigateToCreateCommunity(context),
               ),
-            ), error: (error, stacktrace) => ErrorText(error: error.toString()), loading: () => const Loader())
-          ],
+              
+              ref.watch(userCommunitiesProvider).when(data: (communities) => Expanded(
+                child: ListView.builder(
+                  itemCount: communities.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final community = communities[index];
+                    return ListTile(
+                      title: Text('c/${community.name}'),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(community.avatar),
+                      ),
+                      onTap: (){navigateToCommunity(context, community);},
+                    );
+                  },
+                ),
+              ), error: (error, stacktrace) => ErrorText(error: error.toString()), loading: () => const Loader())
+            ],
+          ),
         ),
       ),
     );
--- lib/features/home/drawers/profile_drawer.dart
+++ lib/features/home/drawers/profile_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
@@ -22,40 +24,43 @@ class ProfileDrawer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
-    return Drawer(
-      child: SafeArea(
-        child: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(user.profilePic),
-              radius: 75,
-            ),
-            const SizedBox(height: 10,),
-            Text(user.name, style: const TextStyle(fontSize: 26),),
-            const SizedBox(height: 10,),
-            const Divider(),
-            ListTile(
-              title: const Text('Profile'),
-              leading: const Icon(Icons.person),
-              onTap: () => navigateToUserProfile(context, user.uid),
-            ),
-            ListTile(
-              title: const Text('Log out'),
-              leading: Icon(Icons.exit_to_app, color: Pallete.redColor,),
-              onTap: () => logOut(ref)
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Icon(Icons.sunny),
-                  Switch.adaptive(value: ref.watch(themeNotifierProvider.notifier).mode == ThemeMode.light ? false : true, onChanged: (value)=> toggleTheme(ref)),
-                  const Icon(Icons.nightlight_round_rounded)
-                ],
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+      child: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.profilePic),
+                radius: 75,
               ),
-            )
-          ],
+              const SizedBox(height: 10,),
+              Text(user.name, style: const TextStyle(fontSize: 26),),
+              const SizedBox(height: 10,),
+              const Divider(),
+              ListTile(
+                title: const Text('Profile'),
+                leading: const Icon(Icons.person),
+                onTap: () => navigateToUserProfile(context, user.uid),
+              ),
+              ListTile(
+                title: const Text('Log out'),
+                leading: Icon(Icons.exit_to_app, color: Pallete.redColor,),
+                onTap: () => logOut(ref)
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Icon(Icons.sunny),
+                    Switch.adaptive(value: ref.watch(themeNotifierProvider.notifier).mode == ThemeMode.light ? false : true, onChanged: (value)=> toggleTheme(ref)),
+                    const Icon(Icons.nightlight_round_rounded)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
